556c76945b720f523e6357974ad695c9e10c69a7
Norbey <norbeymz2000@gmail.com>
2025-04-14 21:09:10 -0500

    primer commit

---
+++ SpringMicroservicios/microservicio-asignaturas/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO asignaturas (nombre, profesor) VALUES ('Matematicas', 'Profesor A');
+INSERT INTO asignaturas (nombre, profesor) VALUES ('Física', 'Profesor B');
+INSERT INTO asignaturas (nombre, profesor) VALUES ('Historia', 'Profesor C');
+INSERT INTO asignaturas (nombre, profesor) VALUES ('Literatura', 'Profesor D');
+INSERT INTO asignaturas (nombre, profesor) VALUES ('Biología', 'Profesor E');
